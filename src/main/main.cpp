240319d7619d831d20a78191727ce6483bd05c77
whpointz <whpointz@163.com>
2017-05-04 21:06:14 +0800

    hello world!

---
+++ src/main/main.cpp
@@ -0,0 +1,17 @@
+/**
+ *    > Author:   whpointz
+ *    > Mail:     506273980@qq.com
+ *    > Github:   https://www.github.com/whpointz
+ *    > Description:
+ *
+ *    > Created Time: 2017/05/04 21:04:32
+**/
+
+#include <stdio.h>
+
+int main(){
+	printf("hello world!\n");
+}
+
+
+
