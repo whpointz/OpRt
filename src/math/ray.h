516d72e047970c52b666e127c683de88e37edf8c
whpointz <whpointz@163.com>
2017-05-07 22:46:38 +0800

    add ray

--- src/math/ray.h
+++ src/math/ray.h
@@ -15,23 +15,29 @@
 namespace OpRt {
 
 class  Ray
-	{
+{
 	public:
 
 		Ray(const Point3d &ori, const Vector3d &dir) :ori_(ori), dir_(dir) { }
+
+		void SetOrigin(const Point3d &ori){ ori_ = ori; }
+		void SetDirection(const Vector3d &dir){ dir_ = dir; }
+		
+		const Point3d & origin() const { return ori_; }
+		const Vector3d & direction() const { return dir_; }
 
 	private:
 
 		Point3d ori_;
 		Vector3d dir_;
 
-	};
+		friend std::ostream& operator << (std::ostream &os, const Ray &ray){
+			return os << "ori: " << ray.ori_ << "dir:" << ray.dir_;
+		}
+
+};
+
 }// namespace OpRt
-
-
-
-
-
 
 
 #endif /* _RAY_H_ */
